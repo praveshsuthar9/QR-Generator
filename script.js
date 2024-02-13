b84935216514fc201a40e8bcce90f55762815bb6
praveshsuthar9 <praveshsuthar9@gmail.com>
2024-02-13 21:47:19 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let api = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=input';
+let imgBox = document.querySelector('.imgBox');
+let qr = document.getElementById('qrImg');
+let qrText = document.getElementById('qrText');
+
+function generateQR(){
+if(qrText.value.length){
+    qr.src= 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + qrText.value;
+imgBox.classList.add('show-img')
+}else{
+    qrText.classList.add('error');
+    setTimeout(() => {
+qrText.classList.remove('error')
+    },1000)
+}
+}
